69e7d6d991fbdce6028b6b626b7b741dcc6a9666
John Maguire <jmaguire@mdsol.com>
2018-03-08 10:09:20 -0500

    started c the hard way

---
+++ c_the_hard_way/exercises/chap1/ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* this is a comment. */
+
+int main(int argc, char *argv[])
+{
+  int distance = 10;
+  char name[4] = "John";
+  
+  // this is also a comment
+  printf("You are %d miles away \n", distance);
+  printf("You are %s miles away \n", name);
+ 
+  return 0;
+}
