0385ec50dc805ebc0f8433c60f1b57c4a486b0a4
John Maguire <jmaguire@mdsol.com>
2019-04-04 17:31:03 -0400

    added second version of char counter

--- the_c_programming_language/chap1/char_counting.c
+++ the_c_programming_language/chap1/char_counting.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* first way to count chars */
+/* first way to count chars 
 main()
 {
   long nc;
@@ -11,4 +11,13 @@ main()
   }
 
   printf("\n%ld\n", nc);
+}
+*/
+
+/* second way to count chars */
+main()
+{
+  double nc;
+  for(nc = 0; getchar() != EOF; ++nc) {}
+  printf("\n%.0f\n", nc);
 }
