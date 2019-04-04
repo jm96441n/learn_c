5ce329ebd3059ccb0aa35a40fed396f2e0f4d5fc
John Maguire <jmaguire@mdsol.com>
2019-04-04 09:47:10 -0400

    added code to convert celsius to fahrenheit

--- the_c_programming_language/chap1/temp_conversion_1_3.c
+++ the_c_programming_language/chap1/temp_conversion_1_3.c
@@ -9,6 +9,7 @@ main()
   upper = 300;
   step = 20;
 
+  /* Convert Fahrenheit to Celsius */
   fahr = lower;
 
   printf("Fahrenheit \t Celsius\n");
@@ -16,5 +17,21 @@ main()
     cel = (5.0 / 9.0) * (fahr - 32.0);
     printf("%3.0f\t\t%6.1f\n", fahr, cel);
     fahr = fahr + step;
+  }
+
+  for(int i = 0; i < 3; i++) {
+    printf("\n");
+  }
+
+
+  /* Convert Celsius to Fahrenheit */
+  cel = lower;
+
+  printf("Celsius \t Fahrenheit\n");
+
+  while(cel <= upper) {
+    fahr = ((9.0 / 5.0) * cel) + 32.0;
+    printf("%3.0f\t\t%6.1f\n", cel, fahr);
+    cel = cel + step;
   }
 }
