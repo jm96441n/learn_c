c8039616b8c247b4019613d0d04a59bf0d6d2c9a
John Maguire <john@jmaguire.tech>
2019-04-04 09:24:23 -0400

    removed old exercises, starting fresh

---
+++ the_c_programming_language/chap1/temp_conversion_1_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main()
+{
+  int fahr, cel;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+
+  while(fahr <= upper) {
+    cel = (5 * (fahr - 32)) / 9;
+    printf("%d\t%d\n", fahr, cel);
+    fahr = fahr + step;
+  }
+}
