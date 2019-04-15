9d49890b81a11e68d93dc303f3b244654beb965f
John Maguire <john@jmaguire.tech>
2019-04-15 17:07:09 -0400

    updated program to print the total length of the longest line even when it exceeds the max

--- the_c_programming_language/chap1/char_arrays_example.c
+++ the_c_programming_language/chap1/char_arrays_example.c
@@ -20,7 +20,13 @@ main()
   }
 
   if (max > 0) {
-    printf("%s", longest);
+    printf("\n Length: %d\n", max);
+    int i = 0;
+    while((i < MAXLINE) && (longest[i] != '\0')) {
+      printf("%c", longest[i]);
+      ++i;
+    }
+    printf("\n");
   }
 
   return 0;
@@ -28,18 +34,25 @@ main()
 
 int getLine(char s[], int lim)
 {
-  int c, i;
+  int c, i, j;
 
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-    s[i] = c;
+  i = 0;
+  j = 0;
+  while((c = getchar()) != EOF && (c != '\n')) {
+    if (i < lim - 1) {
+      s[i] = c;
+      ++i;
+    }
+    ++j;
   }
 
   if (c == '\n') {
     s[i] = c;
     ++i;
   }
+
   s[i] = '\0';
-  return i;
+  return j;
 }
 
 void copy(char to[], char from[])
