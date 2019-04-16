27444aaa2dcb1baf57aed87be6b408a444b0d5a0
John Maguire <john@jmaguire.tech>
2019-04-16 13:58:56 -0400

    handles splitting columns

--- the_c_programming_language/chap1/make_columns.c
+++ the_c_programming_language/chap1/make_columns.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #define COLUMNLENGTH 80
-#define MAXLINE 1000
+#define MAXLINE 8000
+
 main()
 {
-  int currentIndex, lastWhiteSpaceIndex, c;
+  int currentIndex, lastWhiteSpaceIndex, inLineIndex, c, i;
   char line[MAXLINE];
 
-  while((c = getchar()) !=EOF) {
+  currentIndex = 0;
+  lastWhiteSpaceIndex = 0;
+  inLineIndex = 0;
+  while((c = getchar()) != EOF) {
+    if(c == ' ') {
+      lastWhiteSpaceIndex = currentIndex;
+    }
 
+    if (inLineIndex >= COLUMNLENGTH) {
+      line[lastWhiteSpaceIndex] = '\n';
+      inLineIndex = 0;
+    }
+
+    line[currentIndex] = c;
+    ++currentIndex;
+    ++inLineIndex;
+  }
+  for(i = 0; i < currentIndex; ++i) {
+    printf("%c", line[i]);
   }
 }
