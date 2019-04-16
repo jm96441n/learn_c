0c9534c9f461da0201ae544a10249d4e6ea7d541
John Maguire <john@jmaguire.tech>
2019-04-16 16:08:37 -0400

    updated make columns to work with lines without spaces

--- the_c_programming_language/chap1/make_columns.c
+++ the_c_programming_language/chap1/make_columns.c
@@ -2,6 +2,9 @@
 #define COLUMNLENGTH 80
 #define MAXLINE 8000
 
+void splitLineWithoutSpaces(char o[], int len, int lim);
+void copy(char o[], char d[]);
+
 main()
 {
   int currentIndex, lastWhiteSpaceIndex, inLineIndex, c, i;
@@ -10,8 +13,8 @@ main()
   currentIndex = 0;
   lastWhiteSpaceIndex = 0;
   inLineIndex = 0;
-  while((c = getchar()) != EOF) {
-    if(c == ' ') {
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
       lastWhiteSpaceIndex = currentIndex;
     }
 
@@ -24,7 +27,42 @@ main()
     ++currentIndex;
     ++inLineIndex;
   }
-  for(i = 0; i < currentIndex; ++i) {
+  line[++currentIndex] = '\0';
+
+  /* if line has no spaces */
+  if (currentIndex > COLUMNLENGTH && lastWhiteSpaceIndex == 0) {
+    splitLineWithoutSpaces(line, currentIndex, COLUMNLENGTH);
+  }
+
+  for (i = 0; i < currentIndex; ++i) {
     printf("%c", line[i]);
+  }
+  printf("\n");
+}
+
+void splitLineWithoutSpaces(char original[], int originalLength, int limit)
+{
+  char splitLine[MAXLINE];
+  int i, j;
+  j = 0;
+  for (i = 0; i < originalLength; ++i) {
+    if (i % limit == 0 && i != 0) {
+      splitLine[j++] = '-';
+      splitLine[j++] = '\n';
+    }
+    splitLine[j] = original[i];
+    ++j;
+  }
+  splitLine[j] = '\0';
+  copy(splitLine, original);
+}
+
+void copy(char origin[], char destination[])
+{
+  int c, i;
+  i = 0;
+  while((c = origin[i]) != '\0') {
+    destination[i] = origin[i];
+    ++i;
   }
 }
