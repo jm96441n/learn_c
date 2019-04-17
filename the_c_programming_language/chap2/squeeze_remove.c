890b6e562fc449b0e6659e893a78bf7f2b1a334f
John Maguire <john@jmaguire.tech>
2019-04-17 16:43:44 -0400

    added squeeze all function

--- the_c_programming_language/chap2/squeeze_remove.c
+++ the_c_programming_language/chap2/squeeze_remove.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#define MAXLINE 1000
 
 void squeeze_all(char s[], char d[]);
+void squeeze(char s[], char c);
 
 main()
 {
-
+  char line1[MAXLINE];
+  char line2[MAXLINE];
+  printf("Enter your first sentence: ");
+  fgets(line1, sizeof(line1), stdin);
+  printf("Enter your second sentence: ");
+  fgets(line2, sizeof(line2), stdin);
+  squeeze_all(line1, line2);
+  printf("New first sentence: %s\n", line1);
 }
 
-void squeeze_all(char source[], char destination[])
+void squeeze_all(char s1[], char s2[])
 {
+  int i;
+  for (i = 0; s2[i] != '\0'; ++i) {
+    squeeze(s1, s2[i]);
+  }
+}
+
+void squeeze(char line[], char character)
+{
+  int i, j;
+  for (i = j = 0; line[i] != '\0'; i++) {
+    if (line[i] != character) {
+      line[j++] = line[i];
+    }
+  }
+  // new end of line is at j
+  line[j] = '\0';
 }
